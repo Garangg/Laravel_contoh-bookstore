27bcfbe574420d3c9260777c03527b4508c2afdf
Rangga Eka Kusuma Dani <ranggaeka165@gmail.com>
2023-07-15 19:23:11 +0700

    update

--- routes/api.php
+++ routes/api.php
@@ -7,6 +7,8 @@ use App\Http\Resource\Book as BookResource;
 use App\Http\Controllers\BookController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ShopController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -23,20 +25,28 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::prefix('v1')->group(function (){
+Route::prefix('v1')->group(function () {
     Route::post('login', [AuthController::class, 'login']);
     Route::post('register', [AuthController::class, 'register']);
-    Route::post('logout', [AuthController::class, 'logout']);
+
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::post('logout', [AuthController::class, 'logout']);
+        Route::post('shipping', [ShopController::class, 'shipping']);
+        Route::post('services', [ShopController::class, 'services']);
+    });
+
     Route::get('books', [BookController::class, 'index']);
     Route::get('books/{id}', [BookController::class, 'show']);
     Route::get('categories/random/{count}', [CategoryController::class, 'random']);
-    Route::get('books/top/{count}',[BookController::class, 'top']);
+    Route::get('books/top/{count}', [BookController::class, 'top']);
     Route::get('categories', [CategoryController::class, 'index']);
-    Route::get('categories/slug/{slug}',[CategoryController::class, 'slug']);
-    Route::get('books/slug/{slug}',[BookController::class, 'slug']);
-    Route::get('books/search/{keyword}',[BookController::class, 'search']);
-    
-    
+    Route::get('categories/slug/{slug}', [CategoryController::class, 'slug']);
+    Route::get('books/slug/{slug}', [BookController::class, 'slug']);
+    Route::get('books/search/{keyword}', [BookController::class, 'search']);
+    Route::get('provinces', [ShopController::class, 'provinces']);
+    Route::get('cities', [ShopController::class, 'cities']);
+    Route::get('couriers', [ShopController::class, 'couriers']);
+
 });
 
 // Route::get('/books', [BookController::class, 'index']);
